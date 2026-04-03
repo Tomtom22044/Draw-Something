1f29b0ed0589f8ca02def499ee80216d1e46275f
Tomer Neeman <tomerneeman@Tomers-MacBook-Pro.local>
2026-04-03 21:20:17 +0300

    fix(express): update routing wildcard syntax for Express 5 compatibility

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ io.on('connection', (socket) => {
 });
 
 // Any unmatched route will serve the index.html so React Router works
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
